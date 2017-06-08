99428bfc67bd5706948f237e8315e060877020ec
Paul Kotov <paulkotov1@gmail.com>
2017-06-08 21:10:15 +0500

    edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,12 +28,45 @@ export default class Footer extends Component {
       </span>
     )
   }
-
-  renderFilterLink(filter) {
+  
+renderFilterLink(filter) {
     const title = FILTER_TITLES[filter]
     const { filter: selectedFilter, onShow } = this.props
 
     return (
       <a className={classnames({ selected: filter === selectedFilter })}
          style={{ cursor: 'pointer' }}
-         
+         onClick={() => onShow(filter)}>
+        {title}
+      </a>
+    )
+  }
+
+  renderClearButton() {
+    const { completedCount, onClearCompleted } = this.props
+    if (completedCount > 0) {
+      return (
+        <button className="clear-completed"
+                onClick={onClearCompleted} >
+          Clear completed
+        </button>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <footer className="footer">
+        {this.renderTodoCount()}
+        <ul className="filters">
+          {[ SHOW_ALL, DEPARTURE_CITY, ARRIVAL_CITY ].map(filter =>
+            <li key={filter}>
+              {this.renderFilterLink(filter)}
+            </li>
+          )}
+        </ul>
+        {this.renderClearButton()}
+      </footer>
+    )
+  }
+} 
