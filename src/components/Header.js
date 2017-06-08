25686b3670b0c5505a15b0db120a4e4883beac3e
Paul Kotov <paulkotov1@gmail.com>
2017-06-08 18:14:59 +0500

    Header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import TodoTextInput from './TodoTextInput'
+import TextInput from './TextInput'
 
 export default class Header extends Component {
   static propTypes = {
-    addTodo: PropTypes.func.isRequired
+    addFlight: PropTypes.func.isRequired
   }
 
-  handleSave = text => {
+  handler = text => {
     if (text.length !== 0) {
-      this.props.addTodo(text)
+      this.props.addFlight(text)
     }
-  }
+  };
 
   render() {
     return (
       <header className="header">
-        <h1>todos</h1>
-        <TodoTextInput newTodo
-                       onSave={this.handleSave}
-                       placeholder="What needs to be done?" />
+        <h1>Flight panel information</h1>
+        <TextInput newFlight
+                       onSave={this.handler}
+                       placeholder="Please enter your flight information" />
       </header>
     )
   }
