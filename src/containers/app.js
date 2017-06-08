99d5f22723c3af8444583d5772ba33c4e12b9e70
Paul Kotov <paulkotov1@gmail.com>
2017-06-08 16:02:51 +0500

    created app container

---
+++ src/containers/app.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import * as FlightActions from '../actions'
+
+const App = ({flights, actions}) => (
+  <div>
+    <Header addTodo={actions.addTodo} />
+    <MainSection todos={todos} actions={actions} />
+  </div>
+);
+
+App.propTypes = {
+  flights: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(FlightActions, dispatch)
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App) 
